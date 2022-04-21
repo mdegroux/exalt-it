058232c16a9283a88f5e4c36b6eadf3a7c6cb055
Mathieu DEGROUX <mathieu.degroux@ebp.com>
2022-04-21 16:49:11 +0200

    Commit katas

---
+++ SameHashCode/SameHashCode/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SameHashCode
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      var strings = GenerateStrings();
+
+      Console.WriteLine($"Strings \"{strings.s1}\", \"{strings.s2}\", \"{strings.s3}\" have the hashcode {strings.s1.GetHashCode()}");
+
+      Console.WriteLine($"Test {(TestStrings(strings.s1, strings.s2, strings.s3) ? "succeeded" : "failed")}");
+    }
+
+    private static (string s1, string s2, string s3) GenerateStrings()
+    {
+      //The average count of hascodes generated before having a positive match is about 4 000 000
+      var hashCodes = new Dictionary<int, List<string>>(4000000);
+
+      int i = 0;
+
+      while (true)
+      {
+        //Generate a string, we could use other methods such as Random, Path.GetRandomFileName, Guid.NewGuid
+        string str = (i++).ToString();
+        int hashCode = str.GetHashCode();
+
+        //We keep a list of strings for a given hashcode
+        List<string> strings;
+        if (!hashCodes.TryGetValue(hashCode, out strings))
+          hashCodes[hashCode] = strings = new List<string>(3);
+
+        strings.Add(str);
+
+        //Once we have a list of three strings, we have a match
+        if (strings.Count == 3)
+          return (strings[0], strings[1], strings[2]);
+      }
+    }
+
+    private static bool TestStrings(string stringA, string stringB, string stringC)
+    {
+      return stringA.GetHashCode() == stringB.GetHashCode() && !String.Equals(stringA, stringB) &&
+             stringB.GetHashCode() == stringC.GetHashCode() && !String.Equals(stringB, stringC) &&
+             !String.Equals(stringA, stringC);
+
+    }
+  }
+}
---
+++ nthRoot/nthRoot/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace nthRoot
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      TestNthRoot(3404825447, 7, 23);
+      TestNthRoot(203733960.46299, 5, 45.9);
+    }
+
+    public static void TestNthRoot(double number, int n, double result)
+    {
+      var nth = GetNthRoot(number, n);
+      Console.WriteLine($"root {n} of {number} is {nth}");
+      Console.WriteLine($"Test {(nth == result ? "succeeded" : "failed")}");
+    }
+
+
+    public static double GetNthRoot(double number, int n)
+    {
+      //This algorithm is based on Newton's method: https://en.wikipedia.org/wiki/Nth_root#Computing_principal_roots
+
+      // always the given number, simply return it
+      if (n == 1)
+        return number;
+
+      //We initialize the seed (x0)
+      double x = 1;
+
+      //next we iterate until we have a good precision
+      double delta;
+      do
+      {
+        delta = x; // keep previous iteration value
+        x = (((n - 1d) / n) * x) + (number / (n * Pow(x, n - 1))); // compute xn iteration
+        delta -= x; //keep compute delta with previous iteration and current iteration
+      }
+      while (Abs(delta) > 0.0000001); //continue while it's imprecise
+
+      return x;
+    }
+
+    private static double Pow(double number, int n)
+    {
+      double result = 1;
+      for (int i = 1; i <= n; i++)
+      {
+        result *= number;
+      }
+
+      return result;
+    }
+
+    private static double Abs(double number)
+    {
+      if (number > 0)
+        return number;
+
+      return number * -1;
+    }
+  }
+}
